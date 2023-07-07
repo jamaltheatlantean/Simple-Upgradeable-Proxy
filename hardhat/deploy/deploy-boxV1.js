470b65b6f2a0407af6b67606ea4a47a0598e673d
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-07-07 05:36:11 +0100

    add new deploy script

--- hardhat/deploy/deploy-boxV1.js
+++ hardhat/deploy/deploy-boxV1.js
@@ -1,26 +1,10 @@
-const { network } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
-const { verify } = require("../utils/verify")
+const { ethers, upgradeables} = require("hardhat");
 
-module.exports = async ({ getNamedAccounts, deployments}) => {
-    const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
-    const box = await deploy("Box", {
-        from: deployer,
-        args: [],
-        log: true,
-        // wait for live network to verify
-        waitConfirmations: network.config.blockConfirmations || 1,
-    })
-    log(`box deployed at ${box.address}`)
-    console.log("Proxy boxv1 deployed")
+async function main() {
+    const Box = await ethers.getContractFactory("Box");
 
-    if(!developmentChains.includes(network.name) && 
-    process.env.ETHERSCAN_API_KEY
-    ) {
-        await verify(box.address, [INITIAL_SUPPLY])
-    }
+    const box = await upgradeables.deployProxy(Box, [42], {
+        initializer: "initialize",
+    });
+    awiat box.deployed()
 }
-
-module.exports.tags = ["all", "ATN"]
